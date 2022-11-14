65d80343e4ceb8ac1d1a08e2add407da3bba32db
Carolina <carolinabubenik@gmail.com>
2022-11-14 17:12:21 -0300

    Página FAQs funcional.

--- SOB_eCommerce/controllers/mainController.js
+++ SOB_eCommerce/controllers/mainController.js
@@ -1,14 +1,12 @@
+const fs = require('fs');
+
+
 const mainController = {
     faq: (req, res) => {
-        let faqs = [{
-            question: "First Question",
-            answer: "First answer ............."  
-        },
-        {
-            question: "Second Question",
-            answer: "Second answer ............"  
-        }];
-        res.render('main/faq', {faqs}) 
+        let faqFile = fs.readFileSync('./data/faq.json', 'utf8');
+        let faqs = JSON.parse(faqFile);
+        
+        res.render('main/faq', {faqs})
     },
 
     aboutUs: (req, res) => {
