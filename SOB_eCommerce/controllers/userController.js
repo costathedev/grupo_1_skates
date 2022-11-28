cee95b399a6c17d04a71b807b17506d25ed42ef3
Carolina <carolinabubenik@gmail.com>
2022-11-28 15:46:04 -0300

    Nuevas rutas para Crud de User

--- SOB_eCommerce/controllers/userController.js
+++ SOB_eCommerce/controllers/userController.js
@@ -5,7 +5,20 @@ const userController = {
 
     register: function(req, res) {
         res.render('users/register')
-    }
+    },
+
+    index: function(req, res){},
+
+    userDetail: function(req, res){},
+
+    editUser: function(req, res){},
+
+    saveNewUser: function(req, res){},
+
+    saveEditedUser: function(req, res){},
+
+    deleteUser: function(req, res){},
+
 }
 
 module.exports = userController;
--- SOB_eCommerce/routes/user.js
+++ SOB_eCommerce/routes/user.js
@@ -4,8 +4,18 @@ const userController = require ('../controllers/userController');
 
 router.get('/login', userController.login);
 
-
 router.get('/register', userController.register);
 
+router.get('/', userController.index);
+
+router.get('/:id', userController.userDetail);
+
+router.get('/:id/edit', userController.editUser);
+
+router.post('/', userController.saveNewUser);
+
+router.put('/:id', userController.saveEditedUser);
+
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
