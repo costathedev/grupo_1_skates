fedb0931a82746531ecc93d4b36a9afd63f6dd21
Fabian <fabianvelazquez2196@gmail.com>
2022-11-28 23:55:50 -0300

    cambios en rutas

--- SOB_eCommerce/routes/product.js
+++ SOB_eCommerce/routes/product.js
@@ -5,20 +5,26 @@ const productController = require('../controllers/productController');
 // http://localhost:3050/product/productDetail
 
 
-router.get('/', productController.index)
+router.get('/', productController.index) // ok
 
-router.get('/carrodecompras', productController.carroDeCompras);
+router.get('/carrodecompras', productController.carroDeCompras); // ok
 
-router.get('/create', productController.newProduct);
+router.get('/create', productController.newProduct); // render de la vista producto.  ok
 
-router.get('/:id/edit', productController.editProduct);
+router.get('/:id/edit', productController.editProduct);  
+// identificar el id recibido en req.params, buscar en el JSON de productos el que coincida con el ID.
+// renderizar la vista de producto enviando como dato el producto. ok
 
 router.get('/:id', productController.productDetail);
+// identificar el id recibido en req.params, buscar en el JSON de productos el que coincida con el ID.
+// renderizar la vista de producto enviando como dato el producto 
 
 router.post('/', productController.saveNewProduct);
+//Obtener los datos del body, volcarlo en un nuevo objeto producto, agregarlo al array y volver a escribirlo al JSON
 
 router.put('/:id', productController.saveEditedProduct);
-
+//Obtener los datos del JSON, volcarlo en un nuevo objeto producto, agregarlo al array y volver a escribirlo al JSON
+// ..
 router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
