dd406aae51991ecd7f10441f46c571b36f11bc91
Ramiro <108316137+costathedev@users.noreply.github.com>
2023-01-31 00:44:00 -0300

    agregado de ProductColor (no checkeado todavia)
    Todavia no esta revisado si esta bien o no , se debe continuar

---
+++ SOB_eCommerce/database/models/ProductColor.js
@@ -0,0 +1,67 @@
+module.exports = (sequelize, dataTypes) => {
+
+
+    let alias = 'ProductColor';
+    
+    
+    let cols = {
+    
+    
+        id: {
+      
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+            
+        },
+        
+        
+        id_product: {
+        
+        
+            type: dataTypes.INTEGER,
+            
+        },
+        
+        id_color: {
+        
+        
+            type: dataTypes.INTEGER
+            
+        }
+        
+        
+    };
+    
+    
+    let config = {
+        tableName: 'productcolor',
+        timestamps: false
+        
+        
+    };
+
+
+    const ProductColor = sequelize.define(alias, cols, config)
+
+    
+    ProductColor.associate  = function (models){
+    
+    
+        ProductColor.hasMany(models.Color,{
+        
+        
+            as: 'Color',
+            foreignKey:'id',
+            sourceKey: 'id_color',
+            timestamps: false
+        })
+        
+        
+    }   
+    
+    
+    return ProductColor
+    
+    
+}
