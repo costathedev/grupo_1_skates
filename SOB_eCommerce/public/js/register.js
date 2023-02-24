0169b61039e3f14fd8a7cfc0d7405c301f36b881
florlafu <fy.lafuen@gmail.com>
2023-02-23 22:50:02 -0300

    ajustes register

--- SOB_eCommerce/public/js/register.js
+++ SOB_eCommerce/public/js/register.js
@@ -5,35 +5,25 @@ window.onload = function () {
 
 
 
-	let form = document.querySelector(".form-register");
-
-	let nameError = document.getElementById("nameError");
-
-	let surnameError = document.getElementById("surnameError");
-
-	let emailError = document.getElementById("emailError");
-
-	let passwordError = document.getElementById("passwordError");
-
-	let imageError = document.getElementById("imageError");
-
-	let image = document.querySelector("input[type=file]");
+	let form = document.querySelector(".reg-form");
+	let nameError = document.getElementById("firstName");
+	let surnameError = document.getElementById("lastName");
+	let emailError = document.getElementById("email");
+	let passwordError = document.getElementById("password");
+	let imageError = document.getElementById("avatar");
 
 
 	// submit datos
 
 	form.addEventListener("submit", (evento) => {
 		
-		let name = document.getElementById("name");
-		
-		let surname = document.getElementById("apellido");
-		
-		let email = document.getElementById("email");
-	
-		let password = document.getElementById("password");
-		
-		let confirmPassword = document.getElementById("confirmPassword");
+		let name = document.getElementById("firstName");		
+		let surname = document.getElementById("lastName");		
+		let email = document.getElementById("email");	
+		let password = document.getElementById("password");		
+		let confirmPassword = document.getElementById("password_repeated");
 
+		let errores = [];
 
 		//valido el nombre
 	
@@ -70,7 +60,7 @@ window.onload = function () {
 			if (
 		
 				!password.value.matches(
-		
+
 					/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/,
 		
 					"i"
@@ -100,7 +90,7 @@ window.onload = function () {
 	
 		if (!imageValidation) {
 		
-			error.push(
+			errores.push(
 		
 				"Se permiten archivos de formato [PNG, JPG, JPEG, GIF] únicamente."
 		
@@ -112,28 +102,19 @@ window.onload = function () {
 		//Aquí enviamos los errores al usuario
 	
 		let ulErrores = document.getElementById('errores');
-
 		ulErrores.classList.add('alert-danger')
 		
 		if(errores.length > 0){
-			
-			evento.preventDefault();
-		
-			ulErrores.innerHTML = "";
-		
-			for (let i = 0 ; i < errores.length; i++){
-		
-				ulErrores.innerHTML += `<li> ${errores[i]} </li> `
-		
-			}
-		
-			errores = [];
-		
-		}else{
-		
-			return true;
-		
-		} 
+
+			evento.preventDefault();		
+			ulErrores.innerHTML = "";		
+			for (let i = 0 ; i < errores.length; i++){		
+				ulErrores.innerHTML += `<li> ${errores[i]} </li> `		
+			}		
+			errores = [];		
+		}else{		
+			return true;		
+		} 		
 	
 	});
 
