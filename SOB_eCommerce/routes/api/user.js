7f87eacda895c87f9946f57d02411c5824ff987a
Carolina <carolinabubenik@gmail.com>
2023-03-01 23:24:00 -0300

    ..

--- SOB_eCommerce/routes/api/user.js
+++ SOB_eCommerce/routes/api/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userAPIController = require ('../controllers/api/userController');
+const userAPIController = require ('../controllers/api/userAPIController');
 
 router.get('/', userAPIController.list);
 router.get('/:id', userAPIController.show);
